f28c5e3c60d002b059c6771fcfda5e2d954cdcc8
Samuel Simogiarto <34195072+SamuelSiem@users.noreply.github.com>
2020-03-27 21:30:30 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    makesound(this.innerHTML);
+    makeAnnimation(this.innerHTML);
+  });
+}
+
+document.addEventListener("keydown", function(event) {
+  makesound(event.key);
+  makeAnnimation(event.key);
+});
+
+function makesound(key){
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      break;
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      break;
+    case "j":
+      var audio = new Audio("sounds/snare.mp3");
+      break;
+    case "k":
+      var audio = new Audio("sounds/crash.mp3");
+      break;
+    case "l":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      break;
+    default: console.log("not a proper key");
+  }
+  audio.play();
+}
+
+function makeAnnimation(key){
+    document.querySelector("."+key).classList.add("pressed");
+    setTimeout(function(){
+      document.querySelector("."+key).classList.remove("pressed");
+    }, 100);
+}
